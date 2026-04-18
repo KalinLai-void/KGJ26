780114c38a9ab2fae35ab6733ef74703f35e6863
Kalin G. Lai ; 風凜魂太(KalinKonta) <40635383+KalinLai-void@users.noreply.github.com>
2026-04-18 17:34:15 +0800

    refactor: NewInputSystem

--- Assets/Kalin/Scripts/DraggableStationery.cs
+++ Assets/Kalin/Scripts/DraggableStationery.cs
@@ -1,18 +1,42 @@
 ﻿using KalinKonta;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace KalinKonta.Stationery
 {
-    public class DraggableStationery : Stationery, IPointerDownHandler, IDragHandler
+    public class DraggableStationery : Stationery, IPointerDownHandler, IDragHandler, IPointerUpHandler
     {
+        [SerializeField] private float rotationSpeed = 0.05f;
+        private bool isDragging = false;
+
         private void Awake()
         {
             GetComponent<Rigidbody>().isKinematic = true;
         }
 
+        private void Update()
+        {
+            if (isDragging)
+            {
+                HandleScrollRotation();
+            }
+        }
+
+        private void HandleScrollRotation()
+        {
+            Vector2 scroll = InputManager.Instance.ScrollValue;
+
+            if (Mathf.Abs(scroll.y) > 0.01f)
+            {
+                float rotationAmount = scroll.y * rotationSpeed;
+                transform.Rotate(0, 0, rotationAmount);
+            }
+        }
+
         public void OnPointerDown(PointerEventData eventData)
         {
+            isDragging = true;
             Debug.Log($"Click on {gameObject.name}");
         }
 
@@ -21,6 +45,11 @@ namespace KalinKonta.Stationery
             Vector3 screenPos = eventData.position;
             screenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
             transform.position = Camera.main.ScreenToWorldPoint(screenPos);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            isDragging = false;
         }
     }
 }
