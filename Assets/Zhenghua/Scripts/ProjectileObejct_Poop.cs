dd9e93c101c348d0b4e892110173ab56367f1d4a
ZhengHua <aaazzzz168@gmail.com>
2026-04-18 20:49:06 +0800

    feat: 新增便便會隨時間消失的設定

--- Assets/Zhenghua/Scripts/ProjectileObejct_Poop.cs
+++ Assets/Zhenghua/Scripts/ProjectileObejct_Poop.cs
@@ -5,5 +5,24 @@ namespace ZhengHua
 {
     public class ProjectileObject_Poop : ProjectileObject
     {
+        [SerializeField] private float liveTime = 3f;
+        private float _timer = 0f;
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer > liveTime)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _timer = 0f;
+        }
     }
 }
