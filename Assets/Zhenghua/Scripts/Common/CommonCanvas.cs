ddc03a10ac02b174aa447879aa2f73f95c2d4eb0
ZhengHua <aaazzzz168@gmail.com>
2026-04-18 15:22:36 +0800

    feat: 整合相同的程式碼內容。

---
+++ Assets/Zhenghua/Scripts/Common/CommonCanvas.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ZhengHua.Common
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public abstract class CommonCanvas : MonoBehaviour
+    {
+        [SerializeField] protected CanvasGroup _canvasGroup;
+        
+        public virtual void ShowPanel()
+        {
+            _canvasGroup.alpha = 1f;
+            _canvasGroup.blocksRaycasts = true;
+            _canvasGroup.interactable = true;
+        }
+
+        public virtual void HidePanel()
+        {
+            _canvasGroup.alpha = 0f;
+            _canvasGroup.blocksRaycasts = false;
+            _canvasGroup.interactable = false;
+        }
+    }
+}
