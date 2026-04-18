3468e20d3b8414b6ce125d1d887ccfb96fbfe50f
ZhengHua <aaazzzz168@gmail.com>
2026-04-18 15:20:19 +0800

    fix: 修正事件名稱錯誤

--- Assets/Zhenghua/Scripts/GameManager.cs
+++ Assets/Zhenghua/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using ZhengHua.Common;
@@ -8,12 +9,12 @@ namespace ZhengHua
     {
         [SerializeField] private UnityEvent onStage1StartEvent = new();
         [SerializeField] private UnityEvent onStage1FinishEvent = new();
-        public static UnityEvent OnStage1Start() => Instance.onStage1StartEvent;
-        public static UnityEvent OnStage1Finish() => Instance.onStage1FinishEvent; 
+        public static UnityEvent OnStage1Start => Instance.onStage1StartEvent;
+        public static UnityEvent OnStage1Finish => Instance.onStage1FinishEvent; 
         
         [SerializeField] private UnityEvent onStage2StartEvent = new();
         [SerializeField] private UnityEvent onStage2FinishEvent = new();
-        public static UnityEvent OnStage2Start() => Instance.onStage2StartEvent;
-        public static UnityEvent OnStage2Finish() => Instance.onStage2FinishEvent; 
+        public static UnityEvent OnStage2Start => Instance.onStage2StartEvent;
+        public static UnityEvent OnStage2Finish => Instance.onStage2FinishEvent;
     }
 }
